b127f8942f8b5ee582419e9ccae378823cbdb8db
Ray <rayfradella@yaho.com>
2021-03-07 17:03:50 -0500

    Basic AI
    Enemies follow path,
    Player able to shoot,
    Contact with enemy destroys enemy and bullet
    Health system for enemies,
    Damage system for weapons

--- Laser_Defender/Assets/Scripts/Player.cs
+++ Laser_Defender/Assets/Scripts/Player.cs
@@ -7,7 +7,11 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 1f;
     [SerializeField] float topPadding = 5f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 20f;
+    [SerializeField] float projectileFiringDelay = 1f;
 
+    Coroutine firingCoroutine;
     float xMin;
     float xMax;
     float yMin;
@@ -16,22 +20,34 @@ public class Player : MonoBehaviour
     void Start()
     {
         SetUpMoveBoundaries();
-    }
-
-    private void SetUpMoveBoundaries()
-    {
-        Camera gameCamera = Camera.main;
-        
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - topPadding;               
-    }
+        StartCoroutine(displayConsoleColorText());
+    }   
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    //Coroutine - Continues with this logic untill yeild return condition is met
+    IEnumerator displayConsoleColorText()
+    {
+        Debug.Log("<color=red>ERROR</color> ");
+        Debug.Log("<color=yellow>WARNING</color> ");
+        yield return new WaitForSeconds(3);
+        Debug.Log("<color=white>INFORMATION</color> ");
+    }
+    IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(laserPrefab,
+                    transform.position,
+                    Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringDelay);
+        }
     }
     private void Move()
     {
@@ -41,6 +57,27 @@ public class Player : MonoBehaviour
         var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
         var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
-        transform.position = new Vector2(newXPos, newYPos);
+        transform.position = new Vector2(newXPos, newYPos);       
+    }
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuously());            
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            //StopAllCoroutines();
+            StopCoroutine(firingCoroutine);
+        }
+    }
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - topPadding;
     }
 }
